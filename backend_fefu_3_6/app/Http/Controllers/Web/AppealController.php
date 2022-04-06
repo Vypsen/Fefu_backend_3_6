235b57d2a8f603b829b1fd42e8e084974c0a9a51
Vypsen <s.semenets2001@mail.ru>
2022-04-06 21:46:54 +1000

    Lesson 4: create web appeal, validation phone
    Lesson 4: create web appeal, validation phone

---
+++ backend_fefu_3_6/app/Http/Controllers/Web/AppealController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\AppealFormRequest;
+use App\Models\Appeal;
+use App\Sanitizers\PhoneSanitizer;
+use App\Http\Controllers\Controller;
+
+class AppealController extends Controller
+{
+    public function form()
+    {
+        return view('appeal', ['success' => session('success', false)]);
+    }
+
+    public function send(AppealFormRequest $request)
+    {
+        $data = $request->validated();
+
+        $appeal = new Appeal();
+        $appeal->name = $data['name'];
+        $appeal->phone =  PhoneSanitizer::sanitize($data['phone']);
+        $appeal->email = $data['email'];
+        $appeal->message = $data['message'];
+        $appeal->save();
+
+        return redirect(route('appeal.form'))->with(['success' => true]);
+    }
+}
---
+++ backend_fefu_3_6/app/Http/Requests/AppealFormRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\ValidationPhoneNumder;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AppealFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:100'],
+            'phone' => ['nullable', new ValidationPhoneNumder],
+            'email' => ['nullable', 'email:rfc'],
+            'message' => ['required', 'string', 'max:1000'],
+        ];
+    }
+}
